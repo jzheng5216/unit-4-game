e2e7c52730ccd1d282a66d4ed5efa4db3d15ae9e
J Zheng <jzheng5216@gmail.com>
2018-10-20 22:32:59 -0400

    unfinished safety precautions

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    //create characters and set stats
+    let playablechars= {
+        "skeleton":{
+            name: "skeleton",
+            health:100,
+            attack: 5,
+            imagesrc:"assets/images/RunescapeSkele.jpeg",
+            counterattackpower: 10
+        },
+        "baraggan":{
+            name: "baraggan",
+            health: 3000,
+            attack: 0,
+            imagesrc="assets/images/Baraggan.jpeg",
+            counterattackpower: 100
+        },
+        "brook":{
+            name: "brook",
+            health: 1000,
+            attack: 300,
+            imagesrc="assets/images/Brook.jpeg",
+            counterattackpower: 18
+        },
+        "ains":{
+            name: "ains",
+            health: 9999,
+            attack: 999,
+            imagesrc="assets/images/ains.jpeg",
+            counterattackpower: 1,
+        }
+    };
+
+    //necessary variables
+
+}) 
